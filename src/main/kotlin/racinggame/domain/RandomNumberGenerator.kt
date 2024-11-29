ae981d75cd9f755156e2a0fe8ebe0f385f957647
eunice0035 <eunice7357@gmail.com>
2024-11-29 20:23:45 +0900

    refactor: apply Strategy pattern

--- src/main/kotlin/racinggame/domain/RandomNumberGenerator.kt
+++ src/main/kotlin/racinggame/domain/RandomNumberGenerator.kt
@@ -3,11 +3,11 @@ package racinggame.domain
 import kotlin.random.Random
 import kotlin.random.nextInt
 
-object RandomNumberGenerator {
+object RandomNumberGenerator : RandomNumberStrategy {
     private const val START_RANGE = 0
     private const val END_RANGE = 9
 
-    fun generate(): Int {
+    override fun generate(): Int {
         return Random.nextInt(START_RANGE..END_RANGE)
     }
 }
